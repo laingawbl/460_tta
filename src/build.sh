b2d01928af11f00895b64db16cb46ea698df2c48
caseymdk <casey@loopinsights.ca>
2019-04-04 19:37:46 -0700

    Organized more

--- src/build.sh
+++ src/build.sh
@@ -6,7 +6,9 @@ avr-g++ -c -std=c++0x -Os -mmcu=atmega2560 -Wa,--gstabs -o roomba.o ./lib/roomba
 avr-g++ -c -std=c++0x -Os -mmcu=atmega2560 -Wa,--gstabs -o manctl.o ./manctl.cpp
 avr-g++ -c -std=c++0x -Os -mmcu=atmega2560 -Wa,--gstabs -o tta.o ./rtos/tta.cpp
 avr-g++ -c -std=c++0x -Os -mmcu=atmega2560 -Wa,--gstabs -o turretdrv.o ./drive/turretdrv.cpp
-avr-g++ -c -std=c++0x -Os -mmcu=atmega2560 -Wa,--gstabs -o main.o main.cpp
+avr-g++ -c -std=c++0x -Os -mmcu=atmega2560 -Wa,--gstabs -o irdrv.o ./drive/irdrv.cpp
+avr-g++ -c -std=c++0x -Os -mmcu=atmega2560 -Wa,--gstabs -o manctl.o ./manctl.cpp
+avr-g++ -c -std=c++0x -Os -mmcu=atmega2560 -Wa,--gstabs -o main.o ./main.cpp
 avr-g++  -mmcu=atmega2560 -o main.elf *.o
 avr-objcopy -j .text -j .data -O ihex main.elf main.flash.hex
 avrdude -v -patmega2560 -cwiring -P/dev/ttyACM0 -b115200 -D -Uflash:w:main.flash.hex
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include "uart/uart.h"
+#include "lib/roomba.h"
+#include "lib/mcu.h"
+#include "drive/turretdrv.h"
+#include "drive/motiondrv.h"
+#include "drive/commdrv.h"
+#include "drive/irdrv.h"
+#include "rtos/tta.h"
+#include "manctl.h"
 
 int main()
 {
     uart_start(UART_57600);
-    uart_sendstr("Hello!\n");
+    uart_sendstr("*** POWERING ON ***\n");
+    uart_sendstr("*** STARTING IR DRIVER ***\n");
+    irDriverStart((Timing_t) {50, 0});
+    uart_sendstr("*** STARTING MANUAL CONTROL ***\n");
+    manctlStart((Timing_t) {25, 0});
+    //OS_Run();
 }
